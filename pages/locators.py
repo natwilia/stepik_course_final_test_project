530cbecf960de2c24aeb9c6851309eecca271c1f
Test User <testuser@example.com>
2022-07-08 14:31:46 +0300

    Added login page locators and LoginPage methods

---
+++ pages/locators.py
@@ -0,0 +1,16 @@
+from selenium.webdriver.common.by import By
+
+class MainPageLocators():
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    
+class LoginPageLocators():
+    LOGIN_FORM = (By.CSS_SELECTOR, "#login_form")
+    LOGIN_EMAIL_ADDRESS = (By.CSS_SELECTOR, "#id_login-username")
+    LOGIN_PASSWORD = (By.CSS_SELECTOR, "#id_login-password")
+    LOGIN_BUTTON = (By.CSS_SELECTOR, "login_submit")
+    REGISTER_FORM = (By.CSS_SELECTOR, "#register_form")
+    REGISTRATION_EMAIL_ADDRESS = (By.CSS_SELECTOR, "#id_registration-email")
+    REGISTRATION_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password1")
+    REGISTRATION_CONFIRM_PASSWORD = (By.CSS_SELECTOR, "#id_registration-password2")
+    REGISTER_BUTTON = (By.CSS_SELECTOR, "registration_submit")
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
---
+++ pages/login_page.py
@@ -0,0 +1,21 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_LINK), "Login link is not present"
+        login_url = self.browser.find_element(*LoginPageLocators.LOGIN_LINK)
+        login_url.click()
+        assert "login" in self.browser.current_url, "There is no 'login' text in URL"
+        
+    def should_be_login_form(self):
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present"
+
+    def should_be_register_form(self):
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "Registration form is not present"
